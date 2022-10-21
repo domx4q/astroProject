e2b8f6079a3100bad6505fd594d4d94531ad43d1
domx4q <ev3dom@gmx.de>
2022-10-21 21:55:41 +0200

    ar test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,40 @@
 <template>
-  <model-viewer
-      :src="modelSrc"
-      alt="A 3D model of an astronaut"
-      ar
-      ar-modes="webxr scene-viewer quick-look"
-      touch-action="pan-x"
-      camera-controls
-      auto-rotate
-      shadow-intensity="1"
-      exposure="1"
-      environment-image="neutral"
-      background-color="transparent"
-      poster="https://modelviewer.dev/shared-assets/models/Astronaut.png"
-      ios-src="https://modelviewer.dev/shared-assets/models/Astronaut.usdz"
+<!--  <model-viewer-->
+<!--      :src="modelSrc"-->
+<!--      alt="A 3D model of an astronaut"-->
+<!--      ar-->
+<!--      ar-modes="webxr scene-viewer quick-look"-->
+<!--      touch-action="pan-x"-->
+<!--      camera-controls-->
+<!--      auto-rotate-->
+<!--      shadow-intensity="1"-->
+<!--      exposure="1"-->
+<!--      environment-image="neutral"-->
+<!--      background-color="transparent"-->
+<!--      poster="https://modelviewer.dev/shared-assets/models/Astronaut.png"-->
+<!--      ios-src="https://modelviewer.dev/shared-assets/models/Astronaut.usdz"-->
 
-      id="planet"
-      style="width: 100%; height: 100vh"
+<!--      id="planet"-->
+<!--      style="width: 100%; height: 100vh"-->
 
-      @load="updatePlanet">
-    <div class="controls">
-      <input type="file" @change="onFileChange"/>
+<!--      @load="updatePlanet">-->
+<!--    <div class="controls">-->
+<!--      <input type="file" @change="onFileChange"/>-->
+<!--    </div>-->
+<!--    <button slot="ar-button" id="ar-button"></button>-->
+<!--  </model-viewer>-->
+  <model-viewer src="models/astronaut.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1">
+    <div class="progress-bar hide" slot="progress-bar">
+      <div class="update-bar"></div>
     </div>
-    <button slot="ar-button" id="ar-button"></button>
+    <button slot="ar-button" id="ar-button">
+      View in your space
+    </button>
+    <div id="ar-prompt">
+      <img src="https://modelviewer.dev/shared-assets/icons/hand.png">
+    </div>
   </model-viewer>
+
 </template>
 
 <script>
---
+++ startServer.sh
@@ -0,0 +1,1 @@
+serve -s dist/ -l tcp://cloudster.online:3511 --ssl-cert /home/cloudster/certificates/cert.crt --ssl-key /home/cloudster/certificates/cloudster.online.key
