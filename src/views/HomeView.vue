251ee1830cf4c49262272265ed115ea4f6601850
domx4q <ev3dom@gmx.de>
2022-10-22 17:03:17 +0200

    reformated code style in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <model-viewer
       :src="modelSrc"
-      alt="A 3D model of an astronaut"
+      alt="Es ist ein Fehler aufgetreten beim Laden des 3D-Modells."
       ar
       touch-action="pan-y pinch-zoom"
       camera-controls
@@ -22,7 +22,8 @@
       <div id="buttons">
         <input type="file" @change="onFileChange" id="textureInput" accept="image/*" class="button"/>
         <button @click="applyRandomTexture" class="button" id="randomTextureButton">Zufällige Textur</button>
-      </div></div>
+      </div>
+    </div>
   </model-viewer>
 </template>
 
@@ -32,7 +33,7 @@ import('@google/model-viewer')
 export default {
   name: 'HomeView',
   components: {},
-  data () {
+  data() {
     return {
       modelSrc: 'models/sphere.glb',
       planet: null,
@@ -40,6 +41,8 @@ export default {
       currentTexture: "/textures/8k_jupiter.jpg",
 
       textures: [
+        // source: https://www.solarsystemscope.com/textures/
+        // license: CC BY-SA 4.0 (Creative Commons Attribution-ShareAlike 4.0 International) [free for any use]
         "/textures/2k_neptune.jpg",
         "/textures/2k_uranus.jpg",
         "/textures/4k_venus_atmosphere.jpg",
@@ -57,7 +60,8 @@ export default {
     }
   },
   computed: {},
-  mounted () {},
+  mounted() {
+  },
   methods: {
     randomTexture() {
       let curRandom = this.textures[Math.floor(Math.random() * this.textures.length)]
@@ -67,23 +71,23 @@ export default {
       this.currentTexture = curRandom
       return curRandom
     },
-
-    async createAndApplyTexture (image) {
+    async createAndApplyTexture(image) {
       const material = this.planet.model.materials[0]
 
       const texture = await this.planet.createTexture(image)
       material.pbrMetallicRoughness['baseColorTexture'].setTexture(texture)
 
     },
-    applyRandomTexture () {
+    applyRandomTexture() {
       this.createAndApplyTexture(this.randomTexture())
-    }, updatePlanet () {
+    },
+    updatePlanet() {
       this.planet = document.querySelector('model-viewer#planet')
       this.loaded = true
 
       // this.applyRandomTexture();
     },
-    onFileChange (e) {
+    onFileChange(e) {
       const file = e.target.files[0]
       this.createAndApplyTexture(URL.createObjectURL(file))
     }
@@ -92,45 +96,54 @@ export default {
 </script>
 <!--create the style-->
 <style scoped>
-  .button {
-    border-radius: 10px;
-    padding: 10px;
-    margin: 10px;
-    background-color: #fff;
-    color: hsl(197, 45%, 49%);
-    border: none;
-    outline: none;
+.button {
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+  background-color: #fff;
+  color: hsl(197, 45%, 49%);
+  border: none;
+  outline: none;
 
-    transition: background-color 0.2s linear, color 0.2s linear;
+  transition: background-color 0.2s linear, color 0.2s linear;
 
-    box-shadow: 0 0 0 5px hsl(197, 45%, 49%);
-  }
-  #textureInput::file-selector-button {
-    background-color: inherit;
-    color: inherit;
-    border: none;
-    border-radius: 4px;
-    outline: none;
+  box-shadow: 0 0 0 5px hsl(197, 45%, 49%);
+}
 
-  }
-  .button:hover {
-    background-color: hsl(197, 45%, 49%);
-    color: #fff;
-  }
-  input[type='file'] { font-size: 0; }
-  ::file-selector-button { font-size: initial; }
+#textureInput::file-selector-button {
+  background-color: inherit;
+  color: inherit;
+  border: none;
+  border-radius: 4px;
+  outline: none;
 
-  #buttons{
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 100;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-  #randomTextureButton {
-    font-size: 1.05em;
-  }
+}
+
+.button:hover {
+  background-color: hsl(197, 45%, 49%);
+  color: #fff;
+}
+
+input[type='file'] {
+  font-size: 0;
+}
+
+::file-selector-button {
+  font-size: initial;
+}
+
+#buttons {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+#randomTextureButton {
+  font-size: 1.05em;
+}
 </style>
