14450b18d22949aa8885d2f32adaf9e37592c289
domx4q <ev3dom@gmx.de>
2022-11-21 15:16:04 +0100

    tzest

--- updateDaemon.py
+++ updateDaemon.py
@@ -10,21 +10,25 @@ import subprocess
 
 def update():
     print("Updating...")
-    os.system("git pull")
+    os.system("bash updateServer.sh")
     print("Done.")
 
 
 def main():
     while True:
         print("Checking for updates...")
-        p = subprocess.Popen(["git", "fetch"], stdout=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            update()
-        else:
-            print("No updates found.")
+        try:
+            subprocess.check_output("git fetch", shell=True)
+            output = subprocess.check_output("git status", shell=True)
+            output = output.decode("utf-8")
+            if "Your branch is up to date" in output:
+                print("No updates found.")
+            else:
+                update()
+        except subprocess.CalledProcessError as e:
+            print("Error: {}".format(e))
+        print("Sleeping for 5 minutes...")
         time.sleep(300)
-
 
 if __name__ == '__main__':
     main()
