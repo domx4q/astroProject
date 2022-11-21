8d119f54111e19d4f91d5ceb742e2265ba85eeab
domx4q <ev3dom@gmx.de>
2022-11-21 15:54:34 +0100

    trdfzguh

---
+++ updateDaemon.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# while infinite loop
+while true:; do
+  if git diff-index --quiet HEAD --; then
+    ./startServer.sh
+  else
+    ./updateServer.sh
+  fi
+#  sleep 5 minutes
+  sleep 5m
+done
